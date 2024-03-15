b205679974ea505cb7efbd87f7454d4388192f06
lkrkerry <374652306@qq.com>
2024-03-15 13:39:18 +0000

    [Untested]constants updated
    Signed-off-by: lkrkerry <374652306@qq.com>

---
+++ src/test/test_utils.py
@@ -0,0 +1,8 @@
+from .utils import *
+
+def test_constants():
+    assert(1)
+
+
+if __name__ == "__main__":
+    test_constants()
--- src/utils/constants.py
+++ src/utils/constants.py
@@ -2,3 +2,5 @@ import typing, sys, os
 
 PROJECT_DIR = os.path.dirname(sys.argv[0])
 DEBUG:int = 1
+
+DATA_DIR = os.path.join(PROJECT_DIR,"dat")
