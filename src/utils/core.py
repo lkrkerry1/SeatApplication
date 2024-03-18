201dfe04dce95a6283f6ef8b637dbeddf46d2d2f
AFLeartLey <54972556+AFLeartLey@users.noreply.github.com>
2024-03-18 15:25:54 +0800

    [Unfinished]modify seat table into a class

--- src/utils/core.py
+++ src/utils/core.py
@@ -2,74 +2,54 @@ import random
 from typing import *
 from utils import constants, io
 
-def put_musnt(blacklist:Dict[str,str], table:List[List[List[str]]], status:Dict[str, bool]) \
-                                            -> Tuple[List[List[List[str]]],Dict[str, bool]]:
+def put_musnt(seating:io.SeatingTable) -> io.SeatingTable:
     """Put all the blacklisted in the table
-
-    Args:
-        blacklist (Dict[str,str]): the rules given
-        table (List[List[List[str]]]): the sitting tables
-        status (Dict[str, bool]): Whether the student has been put
-
-    Returns:
-        List[List[List[str]]]: the result
-        Dict[str, bool]: Whether the student has been put
     """
-    for i, j in blacklist.items():
-        if status[i] == False:
+    for i, j in seating.rules[io.BLACKLIST].items():
+        if seating.status[i] == False:
             column = -1
             row = -1
             pos = random.randint(0, 1)
-            while (column == -1 and row == -1) or (table[column][row][0] != ""):
-                column = random.randint(0, len(table) - 1) # choose a random group
-                row = random.randint(0, len(table[column]) - 1) # choose a random row
-            table[column][row][0] = i
-        if status[j] == False:
+            while (column == -1 and row == -1) or (seating.table[column][row][0] != ""):
+                column = random.randint(0, len(seating.table) - 1) # choose a random group
+                row = random.randint(0, len(seating.table[column]) - 1) # choose a random row
+            seating.table[column][row][0] = i
+        if seating.status[j] == False:
             column = -1
             row = -1
             pos = random.randint(0, 1)
-            while (column == -1 and row == -1) or (table[column][row][0] != "") \
-                                            or table[column][row][pos^1] == i: # bumped into the blacklist
-                column = random.randint(0, len(table) - 1) # choose a random group
-                row = random.randint(0, len(table[column]) - 1) # choose a random row
-            table[column][row][1] = j
-            status[i] = status[j] = True
-    return table, status
+            while (column == -1 and row == -1) or (seating.table[column][row][0] != "") \
+                                            or seating.table[column][row][pos^1] == i: # bumped into the blacklist
+                column = random.randint(0, len(seating.table) - 1) # choose a random group
+                row = random.randint(0, len(seating.table[column]) - 1) # choose a random row
+            seating.table[column][row][1] = j
+            seating.status[i] = seating.status[j] = True
+    return seating
 
-def put_must(whitelist:Dict[str,str], table:List[List[List[str]]], status:Dict[str, bool]) \
-                                            -> Tuple[List[List[List[str]]],Dict[str, bool]]:
+def put_must(seating:io.SeatingTable)-> io.SeatingTable:
     """Put all the whitelisted in the table
-
-    Args:
-        whitelist (Dict[str,str]): The rules given
-        table (List[List[str]]): The sitting tables
-        status (Dict[str, bool]): Whether the student has been put
-
-    Returns:
-        List[List[List[str]]]: The result
-        Dict[str, bool]: Whether the student has been put
     """
-    for i, j in whitelist.items():
-        if status[i] == True:
-            if status[j] == True:
+    for i, j in seating.rules[io.WHITELIST].items():
+        if seating.status[i] == True:
+            if seating.status[j] == True:
                 continue
             else:
                 raise ValueError("Invalid whitelist trying to put '%s' with two deskmates" %i)
-        elif status[j] == True:
-            if status[i] == True:
+        elif seating.status[j] == True:
+            if seating.status[i] == True:
                 continue
             else:
                 raise ValueError("Invalid whitelist trying to put '%s' with two deskmates" %j)
         column = -1
         row = -1
         desk = [i, j]
-        status[i] = status[j] = True
+        seating.status[i] = seating.status[j] = True
         random.shuffle(desk) # randomize the deskmates
-        while (column == -1 and row == -1) or (table[column][row][0] != ""):
-            column = random.randint(0, len(table) - 1) # choose a random group
-            row = random.randint(0, len(table[column]) - 1) # choose a random row
-        table[column][row] = desk
-    return table, status
+        while (column == -1 and row == -1) or (seating.table[column][row][0] != ""):
+            column = random.randint(0, len(seating.table) - 1) # choose a random group
+            row = random.randint(0, len(seating.table[column]) - 1) # choose a random row
+        seating.table[column][row] = desk
+    return seating
 
 def rand_others(names:List[str], table:List[List[List[str]]], status:Dict[str, bool]) -> List[List[List[str]]]:
     """Put all the others in the given table
@@ -81,7 +61,7 @@ def rand_others(names:List[str], table:List[List[List[str]]], status:Dict[str, b
 
     Returns:
         List[List[List[str]]]: the final sitting table
-    """
+    """ # Todo: use class to rewrite
     random.shuffle(names)
     start_column = 0
     current_name = 0
@@ -102,28 +82,9 @@ def check():
     pass
 
 def rdesk():
-    table = io.read_table()
-    if constants.DEBUG:
-        print("Table", table)
-    names = io.read_names()
-    if constants.DEBUG:
-        print("Names",names)
-    rules = io.read_rules(names)
-    if constants.DEBUG:
-        print("Rules", rules)
-    status = {}
-    for name in names:
-        status[name] = False
-    table, status = put_must(rules[io.WHITELIST], table, status)
-    if constants.DEBUG:
-        print("Have put must", table)
-    table, status = put_musnt(rules[io.BLACKLIST], table, status)
-    if constants.DEBUG:
-        print("Have put mustn't", table)
-    table = rand_others(names, table, status)
-    if constants.DEBUG:
-        print("Have put others", table)
+    seating = io.SeatingTable()
     if(constants.DEBUG):
         check()
-    print("The table: ")
-    io.output(table)
+    # Todo: rewrite generate
+    print(seating)
+    seating.save()
--- src/utils/io.py
+++ src/utils/io.py
@@ -4,86 +4,90 @@ from utils import constants
 WHITELIST = 0
 BLACKLIST = 1
 
-def read_table() -> List[List[List[str]]]:
-    """Reads the table given
+class SeatingTable(): # Todo: finish document
+    def __init__(self,
+                table_path=constants.TABLE_FILE,
+                name_path=constants.NAME_FILE,
+                rule_path=constants.RULES_FILE,
+                output_path=constants.OUTPUT_DIR):
+        self.table_path = table_path
+        self.name_path = name_path
+        self.rule_path = rule_path
+        self.output_path = output_path
+        self.table = []
+        self.names = []
+        self.rules = ({},{})
+        self.status = {}
+        self.read_table()
+        self.read_names()
+        self.read_rules()
 
-    Returns:
-        List[str]: a list of the empty table
-    """
-    table = []
-    table_num = {}
-    with open(constants.TABLE_FILE) as f:
-        for line in f.read().splitlines():
-            key, value = line.split('=', 1)
-            if key == "ColumnOfGroup":
-                table_num["ColumnOfGroup"] = [int(i) for i in value.split(",")]
-                continue
-            table_num[key] = int(value)
-    for i in range(table_num["GroupNum"]):
-        table.append([])
-        for j in range(table_num["ColumnOfGroup"][i]):
-            table[i].append([])
-            for _ in range(table_num["LineOfGroup"]):
-                table[i][j].append("")
-    return table
+    def read_table(self) -> None:
+        """Reads the table given
+        """
+        table = self.table
+        table_num = {}
+        with open(self.table_path) as f:
+            for line in f.read().splitlines():
+                key, value = line.split('=', 1)
+                if key == "ColumnOfGroup":
+                    table_num["ColumnOfGroup"] = [int(i) for i in value.split(",")]
+                    continue
+                table_num[key] = int(value)
+        for i in range(table_num["GroupNum"]):
+            table.append([])
+            for j in range(table_num["ColumnOfGroup"][i]):
+                table[i].append([])
+                for _ in range(table_num["LineOfGroup"]):
+                    table[i][j].append("")
+        self.table = table
 
+    def read_names(self) -> None:
+        """Reads the names of the specified properties
+        """
+        with open(self.name_path) as f:
+            self.names = f.read().splitlines()
+        for i in self.names:
+            self.status[i] = False
 
+    def read_rules(self) -> None:
+        """Reads the rules of the specified properties
+        """
+        rules = self.rules
+        names = self.names
+        with open(self.rule_path) as f:
+            for line in f.read().splitlines():
+                if "[WhiteList]" in line:
+                    idx = 0
+                    continue
+                if "[BlackList]" in line:
+                    idx = 1
+                    continue
+                key, value = line.split(':')
+                if idx == WHITELIST and key in rules[idx]:
+                    raise ValueError("Invalid whitesList rule with key '%s' and value '%s'"% key %value)
+                if idx == BLACKLIST and key in rules[idx] and rules[idx][key] == value:
+                    raise ValueError("Invalid blackList rule being multiple: key '%s' and value '%s'"% key %value)
+                if key not in names:
+                    raise ValueError("Invalid rule with '%s' which is not in names"%key)
+                if value not in names:
+                    raise ValueError("Invalid rule with '%s' which is not in names"%value)
+                rules[idx][key] = value
+        self.rules = rules
 
-def read_names() -> List[str]:
-    """Reads the names of the specified properties
-
-    Returns:
-        List[str]: a list of the names
-    """
-    with open(constants.NAME_FILE) as f:
-        return f.read().splitlines()
-
-def read_rules(names: List[str]) -> Tuple[Dict[str, str]]:
-    """Reads the rules of the specified properties
-
-    Args:
-        names (List[str]): The names given
-
-    Returns:
-        Dict[str, str]: a dict of the rules
-    """
-    rules = ({},{})
-    with open(constants.RULES_FILE) as f:
-        for line in f.read().splitlines():
-            if "[WhiteList]" in line:
-                idx = 0
-                continue
-            if "[BlackList]" in line:
-                idx = 1
-                continue
-            key, value = line.split(':')
-            if idx == WHITELIST and key in rules[idx]:
-                raise ValueError("Invalid whitesList rule with key '%s' and value '%s'"% key %value)
-            if idx == BLACKLIST and key in rules[idx] and rules[idx][key] == value:
-                raise ValueError("Invalid blackList rule being multiple: key '%s' and value '%s'"% key %value)
-            if key not in names:
-                raise ValueError("Invalid rule with '%s' which is not in names"%key)
-            if value not in names:
-                raise ValueError("Invalid rule with '%s' which is not in names"%value)
-            rules[idx][key] = value
-    return rules
-
-def output(table: List[List[List[str]]]) -> None:
-    """Output the table
-
-    Args:
-        table (List[List[List[str]]]): The table given
-    """
-    for i in range(len(table)):
-        for j in range(len(table[i])):
-            for k in range(len(table[i][j])):
-                print(table[i][j][k], end=" ")
-            print()
-        print()
-    with open(constants.OUTPUT_DIR, 'w') as f:
+    def __str__(self) -> str:
+        """Output the table
+        """
+        table = self.table
+        output = ""
         for i in range(len(table)):
             for j in range(len(table[i])):
                 for k in range(len(table[i][j])):
-                    f.write(table[i][j][k] + " ")
-                f.write("\n")
-            f.write("\n")
+                    output += (table[i][j][k] + " ")
+                output += "\n"
+            output += "\n"
+        return output
+    
+    def save(self) -> None: # finish document
+        with open(self.output_path, 'w') as f:
+            f.write(self.__str__)
